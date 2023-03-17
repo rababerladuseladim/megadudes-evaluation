d93c00e1893043aa405e3f72294999f799f71c4b
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2023-03-17 13:36:47 +0100

    use path relative to file instead of relative to workdir in tests

--- .test/unit/test_map_taxids_to_uniprot_accessions.py
+++ .test/unit/test_map_taxids_to_uniprot_accessions.py
@@ -1,44 +1,42 @@
 import os
 import sys
-
 import subprocess as sp
-from tempfile import TemporaryDirectory
 import shutil
-from pathlib import Path, PurePosixPath
+from pathlib import Path
 
 sys.path.insert(0, os.path.dirname(__file__))
 
 import common
 
 
-def test_map_taxids_to_uniprot_accessions():
-    with TemporaryDirectory() as tmpdir:
-        workdir = Path(tmpdir) / "workdir"
-        data_path = PurePosixPath(".test/unit/map_taxids_to_uniprot_accessions/data")
-        expected_path = PurePosixPath(".test/unit/map_taxids_to_uniprot_accessions/expected")
+def test_map_taxids_to_uniprot_accessions(tmpdir):
+    workdir = Path(tmpdir) / "workdir"
+    data_path = Path(__file__).parent / __name__.lstrip("test_")
+    input_path = (data_path / "data").as_posix()
+    expected_path = (data_path / "expected").as_posix()
 
-        # Copy data to the temporary workdir.
-        shutil.copytree(data_path, workdir)
+    # Copy data to the temporary workdir.
+    shutil.copytree(input_path, workdir)
 
-        # dbg
-        print("results/map_taxids_to_uniprot_accessions/tax2accessions.json", file=sys.stderr)
+    # dbg
+    print("results/map_taxids_to_uniprot_accessions/tax2accessions.json", file=sys.stderr)
 
-        # Run the test job.
-        sp.check_output([
-            "python",
-            "-m",
-            "snakemake",
-            "-s",
-            "workflow/rules/map_taxids_to_uniprot_accessions.smk",
-            "results/map_taxids_to_uniprot_accessions/tax2accessions.json",
-            "-j1",
-            "--keep-target-files",
-            "--directory",
-            workdir,
-        ])
+    # Run the test job.
+    sp.check_output([
+        "python",
+        "-m",
+        "snakemake",
+        "-s",
+        "workflow/rules/map_taxids_to_uniprot_accessions.smk",
+        "results/map_taxids_to_uniprot_accessions/tax2accessions.json",
+        "-j1",
+        "--keep-target-files",
+        "--directory",
+        workdir,
+    ])
 
-        # Check the output byte by byte using cmp.
-        # To modify this behavior, you can inherit from common.OutputChecker in here
-        # and overwrite the method `compare_files(generated_file, expected_file),
-        # also see common.py.
-        common.OutputChecker(data_path, expected_path, workdir).check()
+    # Check the output byte by byte using cmp.
+    # To modify this behavior, you can inherit from common.OutputChecker in here
+    # and overwrite the method `compare_files(generated_file, expected_file),
+    # also see common.py.
+    common.OutputChecker(input_path, expected_path, workdir).check()
