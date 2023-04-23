7ca1e039bb962c30526467665d736a56143016cf
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2023-04-23 13:34:26 +0200

    add query db to config file and schema

--- test/unit/test_all.py
+++ test/unit/test_all.py
@@ -1,18 +1,20 @@
 import subprocess as sp
 import shutil
-from pathlib import PurePosixPath
+from pathlib import Path
 from test.unit import common
 
 
 def test_all_dry_run(tmpdir):
-    workdir = tmpdir / "workdir"
-    data_path = PurePosixPath("test/unit/all/data")
-    expected_path = PurePosixPath("test/unit/all/expected")
+    workdir = Path(tmpdir) / "workdir"
+    data_path = Path(__file__).parent / "all"
+    input_path = (data_path / "data").as_posix()
+    expected_path = (data_path / "expected").as_posix()
 
     # Copy data to the temporary workdir.
-    shutil.copytree(data_path, workdir)
+    shutil.copytree(input_path, workdir)
 
     # Run the test job.
+
     sp.check_output([
         "python",
         "-m",
@@ -29,4 +31,4 @@ def test_all_dry_run(tmpdir):
     # To modify this behavior, you can inherit from common.OutputChecker in here
     # and overwrite the method `compare_files(generated_file, expected_file),
     # also see common.py.
-    common.OutputChecker(data_path, expected_path, workdir).check()
+    common.OutputChecker(input_path, expected_path, workdir).check()
