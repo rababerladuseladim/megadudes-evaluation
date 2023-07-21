1b2e5ee5b1be3e0b648db5eb90a9a75e4a51f3ab
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2023-07-21 13:47:39 +0200

    make test_all agnostic of cwd

--- test/unit/test_all.py
+++ test/unit/test_all.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from test.unit import common
 
 
-def test_all_dry_run(tmpdir):
+def test_all_dry_run(tmpdir, workflow_path):
     workdir = Path(tmpdir) / "workdir"
     data_path = Path(__file__).parent / "all"
     input_path = (data_path / "data").as_posix()
@@ -19,6 +19,8 @@ def test_all_dry_run(tmpdir):
         "python",
         "-m",
         "snakemake",
+        "-s",
+        workflow_path / "workflow/Snakefile",
         "all",
         "-j1",
         "--keep-target-files",
