ac42011acc31b6b164a717b67af70bb69db8a372
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2023-12-16 12:38:59 +0100

    download resources with wget

--- download_resources.sh
+++ download_resources.sh
@@ -8,14 +8,13 @@ IFS=$'\n\t'
 mkdir -p resources/uniprot
 UNIPROT_VERSION=$(curl --head -sS https://rest.uniprot.org/uniprotkb/ | grep x-uniprot-release: | sed 's/x-uniprot-release: //');
 echo "Uniprot Release Number: $UNIPROT_VERSION" > resources/uniprot/version.txt;
-curl --output-dir resources/uniprot \
-    --remote-name https://www.uniprot.org/docs/speclist.txt \
-    --remote-name https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/idmapping/idmapping_selected.tab.gz \
-    --remote-name https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz \
-    --remote-name https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz;
+wget -P resources/uniprot https://www.uniprot.org/docs/speclist.txt
+wget -P resources/uniprot https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/idmapping/idmapping_selected.tab.gz
+wget -P resources/uniprot https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+wget -P resources/uniprot https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
 
 # ncbi download
 mkdir -p resources/ncbi
 echo "NCBI data downloaded on: $(date)" > resources/ncbi/version.txt;
-curl https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz | \
+wget qO- https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz | \
         tar -xzf - --directory resources/ncbi nodes.dmp names.dmp
