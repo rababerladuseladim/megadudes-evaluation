cde9bcdf4c7984230519f44ef4361df12e6c0346
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2025-01-03 19:32:17 +0100

    keep headline during sampling of taxons

--- workflow/scripts/sample_taxons.py
+++ workflow/scripts/sample_taxons.py
@@ -5,26 +5,27 @@ import sys
 def sample_taxons(input_file, output_file, log=sys.stderr, count=100):
     """Take a random sample of lines from the provided input file.
 
+    The first line is expected to be the headline and written as first line to the output file.
+
     Args:
-        input_file: path to the inpunt file, must habe at least `count` number of lines
+        input_file: path to the input file, must habe at least `count` + 1 number of lines
         output_file: path to file for writing TaxIDs to, one TaxID per line. Is also used for initializing
             random.seed.
         log: handle for writing the log
         count: number of taxa that should be sampled
     """
     with open(input_file, "r") as file_handle:
-        taxids = [l.strip() for l in file_handle]
-    if count > len(taxids):
+        headline, *lines = file_handle.readlines()
+    if count > len(lines):
         print(
-            f"Number of requested taxids ({count}) is larger than number of available taxids ({len(taxids)}).",
+            f"Number of requested taxids ({count}) is larger than number of available taxids ({len(lines)}).",
             file=log,
         )
         sys.exit(1)
     random.seed(output_file)
-    sample_taxids = random.sample(taxids, count)
+    sample_lines = random.sample(lines, count)
     with open(output_file, "w") as output_handle:
-        for taxid in sample_taxids:
-            output_handle.write(f"{taxid}\n")
+        output_handle.writelines([headline, *sample_lines])
 
 
 if snakemake := globals().get("snakemake"):
