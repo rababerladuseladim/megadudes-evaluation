8a43440052f5cdcf38a12aac91a5e2ee8b0da22a
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2025-12-25 11:40:59 +0100

    replace taxonomic ranks with abbreviations, tweak aesthetics, restrict number of supported megadudes files to one

--- workflow/scripts/plot_megadudes_qc.py
+++ workflow/scripts/plot_megadudes_qc.py
@@ -17,7 +17,16 @@ except ImportError:
 
 
 TAX_LEVELS = ["superkingdom", "phylum", "class", "order", "family", "genus", "species", "subspecies"]
-
+TAXON_ABBREVIATIONS = {
+    "superkingdom": "SK",
+    "kingdom": "K",
+    "phylum": "P",
+    "class": "C",
+    "order": "O",
+    "family": "F",
+    "genus": "G",
+    "species": "S",
+}
 
 def read_ground_truth_file(ground_truth_file):
     df_t_taxids = pd.read_csv(
@@ -145,23 +154,30 @@ def calc_eval_metrics(df_hits: pd.DataFrame) -> pd.DataFrame:
     df_fdr_g = grouped.apply(calculate_fdr)
     df_eval = pd.DataFrame(
         {
-            "sensitivity": df_sens_g,
-            "precision": df_precision_g,
+            "Sensitivity": df_sens_g,
+            "Precision": df_precision_g,
             "F1-score": df_f_one_g,
-            "fdr": df_fdr_g,
+            "FDR": df_fdr_g,
         }
     )
     df_eval = df_eval.reset_index()
     return df_eval
 
 
-def plot_qc(df_plt, output):
-    metrics = ["sensitivity", "precision", "F1-score"]
-    f, axes = plt.subplots(ncols=3, figsize=(15, 9), sharex="all", sharey="all")
+def plot_qc(df_plt):
+    # context keywords: notebook, talk, paper, poster
+    sns.set_theme(
+        context="paper",
+        style="whitegrid",
+        rc={"axes.grid.axis": "y"},
+    )
+    plt.rcParams["figure.figsize"] = [16 * 0.4, 9 * 0.4]
+    metrics = ["Sensitivity", "Precision", "F1-score"]
+    f, axes = plt.subplots(ncols=3, sharex="all", sharey="all")
     for metric, ax in zip(metrics, axes):
         sns.lineplot(
             data=df_plt,
-            x="taxon_level",
+            x="taxon_abbreviation",
             y=metric,
             hue="method",
             ax=ax,
@@ -170,12 +186,14 @@ def plot_qc(df_plt, output):
             marker="s",
         )
         ax.set_title(metric)
-        ax.set_ylim(0, 105)
-        plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-        ax.xaxis.label.set_visible(False)
+        ax.set_ylim(-5, 105)
+        ax.set_xlabel("Taxonomic rank")
+        # x-axis label only in second plot
+        ax.xaxis.label.set_visible(metric == metrics[1])
     axes[0].yaxis.label.set_visible(False)
     axes[-1].tick_params(labelright=True)
-    f.savefig(output)
+    sns.despine(left=True, bottom=True)
+    f.tight_layout()
     return f
 
 
@@ -186,28 +204,26 @@ def qc_plots(
     output: str,
     diamond_file: str | None = None
 ):
-    # context keywords: notebook, talk, paper, poster
-    sns.set_theme(context="talk", rc={"axes.grid": False})
-    plt.rcParams["figure.figsize"] = [16 * 0.6, 9 * 0.6]
     hits: list[pd.DataFrame] = []
     # read ground truth
     df_gt_taxids = read_ground_truth_file(ground_truth_file)
     # get diamond hits
-    df_dmnd = get_diamond_hit_counts(diamond_file, df_gt_taxids) if diamond_file else pd.DataFrame()
-    if not df_dmnd.empty:
-        df_dmnd["method"] = "diamond"
-    hits.append(df_dmnd)
+    if diamond_file:
+        df_dmnd = get_diamond_hit_counts(diamond_file, df_gt_taxids)
+        df_dmnd["method"] = "DIAMOND"
+        hits.append(df_dmnd)
     # get unipept hits
     df_uni = get_unipept_hit_counts(unipept_file, df_gt_taxids)
-    df_uni["method"] = "unipept"
+    df_uni["method"] = "Unipept"
     hits.append(df_uni)
     # get megadudes hits
-    for megadudes_file in megadudes_file_list:
-        df_mega = get_megadudes_hit_counts(
-                megadudes_file, df_gt_taxids
-            )
-        df_mega["method"] = "megadudes on " + Path(megadudes_file).parent.name
-        hits.append(df_mega)
+    if len(megadudes_file_list) != 1:
+        raise ValueError("'megadudes_file_list' must contain exactly one file")
+    df_mega = get_megadudes_hit_counts(
+            megadudes_file_list[0], df_gt_taxids
+        )
+    df_mega["method"] = "DUDes"
+    hits.append(df_mega)
     # build TP/FP/FN dataframe
     df_hits = pd.concat(hits, ignore_index=True)
     # build evaluation dataframe, containing sensitivity, precision, f1-score, fdr
@@ -215,19 +231,26 @@ def qc_plots(
     # plot
     # drop subspecies, as no method has any true hits on this level
     df_plt = df_eval[df_eval["taxon_level"] != "subspecies"]
-    return plot_qc(df_plt, output), df_eval
+    df_plt["taxon_abbreviation"] = df_plt["taxon_level"].map(TAXON_ABBREVIATIONS)
+    f = plot_qc(df_plt)
+    f.savefig(output)
+    return f, df_eval
 
 
 def test_qc_plots(tmpdir):
-    ground_truth = "/home/hennings/Projects/megadudes-evaluation/resources/kleiner_ground_truth-equal_protein-full_taxid_lineage.csv"
-    unipept_file = "/home/hennings/Nextcloud/Notes/project support material/taxfdr/files/01-researchgap/02-tax_analysis/unipept_results-Kleiner_P_msfragger.csv"
-    megadudes_file = "/home/hennings/Nextcloud/Notes/project support material/taxfdr/files/02-megadudes/v0.9/megadudes-result.out"
+    import test
+    test_data_directory = Path(test.__file__).parent / "scripts" / "plot_megadudes_qc" / "test_data"
+    ground_truth = test_data_directory / "ground_truth.csv"
+    unipept_file = test_data_directory / "unipept.tsv"
+    diamond_file = test_data_directory / "diamond.tsv"
+    megadudes_file = test_data_directory / "megadudes.tsv"
     output_plot = tmpdir / "qc_plot.svg"
     print(output_plot)
     f, df_eval = qc_plots(
-        ground_truth_file=ground_truth,
-        unipept_file=unipept_file,
-        megadudes_file_list=[megadudes_file],
+        ground_truth_file=ground_truth.as_posix(),
+        diamond_file=diamond_file.as_posix(),
+        unipept_file=unipept_file.as_posix(),
+        megadudes_file_list=[megadudes_file.as_posix()],
         output=output_plot,
     )
     f.show()
